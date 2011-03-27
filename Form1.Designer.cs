151b40b4e5a9b1ecd4cc9417e1f114579a1f5c1d
Alexandr Nikitin <alex.nikitin@mail.ru>
2011-03-27 20:03:12 +0400

    Simple Windows Mobile 6.5 Countdown project

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CountDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.label1 = new System.Windows.Forms.Label();
+            this.start_bt = new System.Windows.Forms.Button();
+            this.stop_bt = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.timer1 = new System.Windows.Forms.Timer();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.Text = "Select time (hh:mm)";
+            // 
+            // start_bt
+            // 
+            this.start_bt.Enabled = false;
+            this.start_bt.Location = new System.Drawing.Point(4, 51);
+            this.start_bt.Name = "start_bt";
+            this.start_bt.Size = new System.Drawing.Size(102, 36);
+            this.start_bt.TabIndex = 1;
+            this.start_bt.Text = "Start";
+            this.start_bt.Click += new System.EventHandler(this.start_bt_Click);
+            // 
+            // stop_bt
+            // 
+            this.stop_bt.Enabled = false;
+            this.stop_bt.Location = new System.Drawing.Point(135, 51);
+            this.stop_bt.Name = "stop_bt";
+            this.stop_bt.Size = new System.Drawing.Size(102, 36);
+            this.stop_bt.TabIndex = 2;
+            this.stop_bt.Text = "Stop";
+            this.stop_bt.Click += new System.EventHandler(this.stop_bt_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "hh:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(135, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(91, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.Value = new System.DateTime(2011, 3, 26, 0, 1, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.stop_bt);
+            this.Controls.Add(this.start_bt);
+            this.Controls.Add(this.label1);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Count down";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button start_bt;
+        private System.Windows.Forms.Button stop_bt;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CountDown
+{
+    public partial class Form1 : Form
+    {
+        int minutes;     
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_bt_Click(object sender, EventArgs e) //Start button was pressed
+        {
+            timer1.Interval = 3600;     //Milliseconds in 1 minute
+            timer1.Enabled = true;      //Start the timer
+            stop_bt.Enabled = true;
+            start_bt.Enabled = false;
+        }
+
+        private void stop_bt_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer1.Dispose();
+            start_bt.Enabled = true;
+            stop_bt.Enabled = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            start_bt.Enabled = true;
+        }
+    }
+}
